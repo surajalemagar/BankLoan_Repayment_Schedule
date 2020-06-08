48dc710c3a28fc8b06f2667f88e49018b997317f
suraj ale magar <surajido111@gmail.com>
2020-06-08 17:57:33 +0545

    Modified

--- BankPaymentRule.py
+++ BankPaymentRule.py
@@ -1,23 +1,37 @@
-time=120
-principal=float(2000000)
-payment=float(31150)
-rate=14
-rate_month=float(14/1200)
-total=float(0)
-total_interest=float(0)
-total_interest=float(0)
-for i in range(120):
-    interest=float(rate_month*principal)
-    deducted_principal=payment-interest
-    principal-=deducted_principal
-    total_interest+=interest
-    i=i+1
-    print(i)
-    print("Payment is :",payment)
-    print("Interest is :",interest)
-    print("Deducted amount in Principal is :",deducted_principal)
-    print("New Principal is :",principal)
-    print("Total Interest Till Now is :",total_interest)
-    print("\n")
+# Create Bank Loan Repayment Schedule
+# Calculate Required Monthly Repayment Amount 
 
 
+def Calculate_Monthly_Payment(P,T,R):
+    RY=float(R/100)             #Calculating Yearly Rate of Interest
+    RM=float(RY/12)             #Calculating Monthly Rate of Interest
+    N=float(T*12)               #Calculating Number of Period
+    x=(RM*P)/(1-(1+RM)**(-N))   #Calculating Required Monthly Payment Amount
+    return x
+
+def Repayment_Schedule(P,T,R,MP):
+    Total_Interest=float(0)
+    Total_Payment=float(0)
+    RY=float(R/100)             #Calculating Yearly Rate of Interest
+    RM=float(RY/12)             #Calculating Monthly Rate of Interest
+    N=float(T*12)  
+    for i in range(int(N)):
+        I=float(RM*P)
+        Deducted_P=MP-I
+        P-=Deducted_P
+        Total_Interest+=I
+        Total_Payment+=MP
+        i=i+1
+        print(i)
+        print("Payment is :",MP)
+        print("Interest is :",I)
+        print("Deducted amount in Principal is :",Deducted_P)
+        print("New Principal is :",P)
+        print("Total Interest Till Now is :",Total_Interest)
+        print("Total Payment Till Now is :",Total_Payment)
+        print("\n")
+P=float(input("Enter The Principal :"))
+T=float(input("Enter The Years :"))
+R=float(input("Enter The Rate :"))
+MP=Calculate_Monthly_Payment(P,T,R)
+Repayment_Schedule(P,T,R,MP)
